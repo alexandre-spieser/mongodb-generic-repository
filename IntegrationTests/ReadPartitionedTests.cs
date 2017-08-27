bd69797d572d96549d1d0781898fd6ce95472b07
alexandre-spieser <bl4ck_dr4g0n2004@hotmail.com>
2017-08-27 18:46:42 +0000

    more tests to validate partitioned collection support

--- IntegrationTests/ReadPartitionedTests.cs
+++ IntegrationTests/ReadPartitionedTests.cs
@@ -6,163 +6,162 @@ using System.Threading.Tasks;
 
 namespace IntegrationTests
 {
-    public class ReadTestsDocument : Document
+    public class ReadTestsPartitionedDocument : PartitionedDocument
     {
-        public ReadTestsDocument()
+        public ReadTestsPartitionedDocument() : base("TestPartitionKey")
         {
-            Version = 2;
+            Version = 1;
         }
         public string SomeContent { get; set; }
     }
 
-    [TestFixture]
-    public class ReadTests : BaseMongoDbRepositoryTests<ReadTestsDocument>
+    public class ReadPartitionedTests : BaseMongoDbRepositoryTests<ReadTestsPartitionedDocument>
     {
         [Test]
-        public async Task GetById()
+        public async Task PartitionedGetById()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = await SUT.GetByIdAsync<ReadTestsDocument>(document.Id);
+            var result = await SUT.GetByIdAsync<ReadTestsPartitionedDocument>(document.Id, PartitionKey);
             // Assert
             Assert.IsNotNull(result);
         }
 
         [Test]
-        public async Task GetOneAsync()
+        public async Task PartitionedGetOneAsync()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = await SUT.GetOneAsync<ReadTestsDocument>(x => x.Id == document.Id);
+            var result = await SUT.GetOneAsync<ReadTestsPartitionedDocument>(x => x.Id == document.Id, PartitionKey);
             // Assert
             Assert.IsNotNull(result);
         }
 
         [Test]
-        public void GetOne()
+        public void PartitionedGetOne()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = SUT.GetOne<ReadTestsDocument>(x => x.Id == document.Id);
+            var result = SUT.GetOne<ReadTestsPartitionedDocument>(x => x.Id == document.Id, PartitionKey);
             // Assert
             Assert.IsNotNull(result);
         }
 
         [Test]
-        public void GetCursor()
+        public void PartitionedGetCursor()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var cursor = SUT.GetCursor<ReadTestsDocument>(x => x.Id == document.Id);
+            var cursor = SUT.GetCursor<ReadTestsPartitionedDocument>(x => x.Id == document.Id, PartitionKey);
             var count = cursor.Count();
             // Assert
             Assert.AreEqual(1, count);
         }
 
         [Test]
-        public async Task AnyAsyncReturnsTrue()
+        public async Task PartitionedAnyAsyncReturnsTrue()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = await SUT.AnyAsync<ReadTestsDocument>(x => x.Id == document.Id);
+            var result = await SUT.AnyAsync<ReadTestsPartitionedDocument>(x => x.Id == document.Id, PartitionKey);
             // Assert
             Assert.AreEqual(true, result);
         }
 
         [Test]
-        public async Task AnyAsyncReturnsFalse()
+        public async Task PartitionedAnyAsyncReturnsFalse()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = await SUT.AnyAsync<ReadTestsDocument>(x => x.Id == Guid.NewGuid());
+            var result = await SUT.AnyAsync<ReadTestsPartitionedDocument>(x => x.Id == Guid.NewGuid(), PartitionKey);
             // Assert
             Assert.AreEqual(false, result);
         }
 
         [Test]
-        public void AnyReturnsTrue()
+        public void PartitionedAnyReturnsTrue()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = SUT.Any<ReadTestsDocument>(x => x.Id == document.Id);
+            var result = SUT.Any<ReadTestsPartitionedDocument>(x => x.Id == document.Id, PartitionKey);
             // Assert
             Assert.AreEqual(true, result);
         }
 
         [Test]
-        public void AnyReturnsFalse()
+        public void PartitionedAnyReturnsFalse()
         {
             // Arrange
             var document = CreateTestDocument();
             SUT.AddOne(document);
             // Act
-            var result = SUT.Any<ReadTestsDocument>(x => x.Id == Guid.NewGuid());
+            var result = SUT.Any<ReadTestsPartitionedDocument>(x => x.Id == Guid.NewGuid(), PartitionKey);
             // Assert
             Assert.AreEqual(false, result);
         }
 
         [Test]
-        public async Task GetAllAsync()
+        public async Task PartitionedGetAllAsync()
         {
             // Arrange
             var documents = CreateTestDocuments(5);
             documents.ForEach(e => e.SomeContent = "GetAllAsyncContent");
             SUT.AddMany(documents);
             // Act
-            var result = await SUT.GetAllAsync<ReadTestsDocument>(x => x.SomeContent == "GetAllAsyncContent");
+            var result = await SUT.GetAllAsync<ReadTestsPartitionedDocument>(x => x.SomeContent == "GetAllAsyncContent", PartitionKey);
             // Assert
             Assert.AreEqual(5, result.Count);
         }
 
         [Test]
-        public void GetAll()
+        public void PartitionedGetAll()
         {
             // Arrange
             var documents = CreateTestDocuments(5);
             documents.ForEach(e => e.SomeContent = "GetAllContent");
             SUT.AddMany(documents);
             // Act
-            var result = SUT.GetAll<ReadTestsDocument>(x => x.SomeContent == "GetAllContent");
+            var result = SUT.GetAll<ReadTestsPartitionedDocument>(x => x.SomeContent == "GetAllContent", PartitionKey);
             // Assert
             Assert.AreEqual(5, result.Count);
         }
 
         [Test]
-        public async Task CountAsync()
+        public async Task PartitionedCountAsync()
         {
             // Arrange
             var documents = CreateTestDocuments(5);
             documents.ForEach(e => e.SomeContent = "CountAsyncContent");
             SUT.AddMany(documents);
             // Act
-            var result = await SUT.CountAsync<ReadTestsDocument>(x => x.SomeContent == "CountAsyncContent");
+            var result = await SUT.CountAsync<ReadTestsPartitionedDocument>(x => x.SomeContent == "CountAsyncContent", PartitionKey);
             // Assert
             Assert.AreEqual(5, result);
         }
 
         [Test]
-        public void Count()
+        public void PartitionedCount()
         {
             // Arrange
             var documents = CreateTestDocuments(5);
             documents.ForEach(e => e.SomeContent = "CountContent");
             SUT.AddMany(documents);
             // Act
-            var result = SUT.Count<ReadTestsDocument>(x => x.SomeContent == "CountContent");
+            var result = SUT.Count<ReadTestsPartitionedDocument>(x => x.SomeContent == "CountContent", PartitionKey);
             // Assert
             Assert.AreEqual(5, result);
         }
