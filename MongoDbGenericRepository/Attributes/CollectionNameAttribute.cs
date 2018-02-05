c117bf2a7fee378f1e02199dea9b2023a7089ee2
Andrea Bertoldo <andrea.bertoldo.btf@outlook.com>
2018-02-05 14:48:26 +0100

    Add CollectionName attribute for allowing explicit naming of collections

---
+++ MongoDbGenericRepository/Attributes/CollectionNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MongoDbGenericRepository.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	public class CollectionNameAttribute : Attribute
+	{
+		public string Name { get; set; }
+
+		public CollectionNameAttribute(string name)
+		{
+			this.Name = name;
+		}
+	}
+}
