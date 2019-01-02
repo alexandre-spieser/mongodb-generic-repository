eaa90d67f5d06313869adf9588e06c9c805a6d65
Alexandre SPIESER <a.spieser@gmail.com>
2019-01-02 23:43:37 +0000

    sum for decimal fields, untested.

---
+++ MongoDbGenericRepository/BaseMongoDbRepository.Maths.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDbGenericRepository.Models;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MongoDbGenericRepository
+{
+    /// <summary>
+    /// The base Repository, it is meant to be inherited from by your custom custom MongoRepository implementation.
+    /// Its constructor must be given a connection string and a database name.
+    /// </summary>
+    public abstract partial class BaseMongoRepository : ReadOnlyMongoRepository, IBaseMongoRepository
+    {
+        /// <summary>
+        /// Sums the values of a selected field for a given filtered collection of documents.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="selector">The field you want to sum.</param>
+        /// <param name="partitionKey">The partition key of your document, if any.</param>
+        public virtual async Task<decimal> SumByAsync<TDocument>(Expression<Func<TDocument, bool>> filter,
+                                                       Expression<Func<TDocument, decimal>> selector,
+                                                       string partitionKey = null)
+                                                       where TDocument : IDocument
+        {
+            var collection = string.IsNullOrEmpty(partitionKey) ? GetCollection<TDocument>() : GetCollection<TDocument>(partitionKey);
+
+            return await collection.AsQueryable()
+                                   .Where(filter)
+                                   .SumAsync(selector);
+        }
+
+    }
+}
