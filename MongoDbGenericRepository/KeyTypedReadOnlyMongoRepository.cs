b77978e57a8d8ff427cf11a9eb1909f199f943ba
Alexandre SPIESER <a.spieser@gmail.com>
2019-02-07 08:47:27 +0000

    introucting KeyTypedReadOnlyMongoRepository

---
+++ MongoDbGenericRepository/KeyTypedReadOnlyMongoRepository.cs
@@ -0,0 +1,331 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDbGenericRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MongoDbGenericRepository
+{
+    /// <summary>
+    /// The base Repository, it is meant to be inherited from by your custom custom MongoRepository implementation.
+    /// Its constructor must be given a connection string and a database name.
+    /// </summary>
+    public abstract class KeyTypedReadOnlyMongoRepository<TKey> where TKey : IEquatable<TKey>
+    {
+
+        /// <summary>
+        /// The connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// The database name.
+        /// </summary>
+        public string DatabaseName { get; set; }
+
+        /// <summary>
+        /// The MongoDbContext
+        /// </summary>
+        protected IMongoDbContext MongoDbContext = null;
+
+        /// <summary>
+        /// The constructor taking a connection string and a database name.
+        /// </summary>
+        /// <param name="connectionString">The connection string of the MongoDb server.</param>
+        /// <param name="databaseName">The name of the database against which you want to perform operations.</param>
+        protected KeyTypedReadOnlyMongoRepository(string connectionString, string databaseName)
+        {
+            MongoDbContext = new MongoDbContext(connectionString, databaseName);
+        }
+
+        #region Read
+
+        /// <summary>
+        /// Asynchronously returns one document given its id.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="id">The Id of the document you want to get.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public async Task<TDocument> GetByIdAsync<TDocument>(TKey id, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            var filter = Builders<TDocument>.Filter.Eq("Id", id);
+            return await HandlePartitioned<TDocument>(partitionKey).Find(filter).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Returns one document given its id.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="id">The Id of the document you want to get.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public TDocument GetById<TDocument>(TKey id, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            var filter = Builders<TDocument>.Filter.Eq("Id", id);
+            return HandlePartitioned<TDocument>(partitionKey).Find(filter).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Asynchronously returns one document given an expression filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public async Task<TDocument> GetOneAsync<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return await HandlePartitioned<TDocument>(partitionKey).Find(filter).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Returns one document given an expression filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public TDocument GetOne<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return HandlePartitioned<TDocument>(partitionKey).Find(filter).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns a collection cursor.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public IFindFluent<TDocument, TDocument> GetCursor<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return HandlePartitioned<TDocument>(partitionKey).Find(filter);
+        }
+
+        /// <summary>
+        /// Returns true if any of the document of the collection matches the filter condition.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public async Task<bool> AnyAsync<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            var count = await HandlePartitioned<TDocument>(partitionKey).CountDocumentsAsync(filter);
+            return (count > 0);
+        }
+
+        /// <summary>
+        /// Returns true if any of the document of the collection matches the filter condition.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public bool Any<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            var count = HandlePartitioned<TDocument>(partitionKey).CountDocuments(filter);
+            return (count > 0);
+        }
+
+        /// <summary>
+        /// Asynchronously returns a list of the documents matching the filter condition.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public async Task<List<TDocument>> GetAllAsync<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return await HandlePartitioned<TDocument>(partitionKey).Find(filter).ToListAsync();
+        }
+
+        /// <summary>
+        /// Returns a list of the documents matching the filter condition.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public List<TDocument> GetAll<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return HandlePartitioned<TDocument>(partitionKey).Find(filter).ToList();
+        }
+
+        /// <summary>
+        /// Asynchronously counts how many documents match the filter condition.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partitionKey</param>
+        public async Task<long> CountAsync<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return await HandlePartitioned<TDocument>(partitionKey).CountDocumentsAsync(filter);
+        }
+
+        /// <summary>
+        /// Counts how many documents match the filter condition.
+        /// </summary>
+        /// <typeparam name="TDocument">The type representing a Document.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="partitionKey">An optional partitionKey</param>
+        public long Count<TDocument>(Expression<Func<TDocument, bool>> filter, string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return HandlePartitioned<TDocument>(partitionKey).Find(filter).CountDocuments();
+        }
+
+        /// <summary>
+        /// Gets the document with the maximum value of a specified property in a MongoDB collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="maxValueSelector">A property selector to order by descending.</param>
+        /// <param name="partitionKey">An optional partitionKey.</param>
+        public async Task<TDocument> GetByMaxAsync<TDocument>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, object>> maxValueSelector, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return await GetByMaxAsync<TDocument>(filter, maxValueSelector, partitionKey);
+        }
+
+        /// <summary>
+        /// Gets the document with the maximum value of a specified property in a MongoDB collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="maxValueSelector">A property selector to order by descending.</param>
+        /// <param name="partitionKey">An optional partitionKey.</param>
+        /// <returns></returns>
+        public TDocument GetByMax<TDocument>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, object>> maxValueSelector, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return GetByMax<TDocument>(filter, maxValueSelector, partitionKey);
+        }
+
+        /// <summary>
+        /// Gets the document with the maximum value of a specified property in a MongoDB collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="minValueSelector">A property selector to order by ascending.</param>
+        /// <param name="partitionKey">An optional partitionKey.</param>
+        public async Task<TDocument> GetByMinAsync<TDocument>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, object>> minValueSelector, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return await GetByMinAsync<TDocument>(filter, minValueSelector, partitionKey);
+        }
+
+        /// <summary>
+        /// Gets the document with the maximum value of a specified property in a MongoDB collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="orderByAscending">A property selector to order by ascending.</param>
+        /// <param name="partitionKey">An optional partitionKey.</param>
+        public TDocument GetByMin<TDocument>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, object>> orderByAscending, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return GetByMin<TDocument>(filter, orderByAscending, partitionKey);
+        }
+
+        /// <summary>
+        /// Gets the maximum value of a property in a mongodb collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <typeparam name="TValue">The type of the value used to order the query.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="maxValueSelector">A property selector to order by ascending.</param>
+        /// <param name="partitionKey">An optional partitionKey.</param>
+        public async Task<TValue> GetMaxValueAsync<TDocument, TValue>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, TValue>> maxValueSelector, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return await GetMaxValueAsync<TDocument, TValue>(filter, maxValueSelector, partitionKey);
+        }
+
+        /// <summary>
+        /// Gets the maximum value of a property in a mongodb collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <typeparam name="TValue">The type of the value used to order the query.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="maxValueSelector">A property selector to order by ascending.</param>
+        /// <param name="partitionKey">An optional partitionKey.</param>
+        public TValue GetMaxValue<TDocument, TValue>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, TValue>> maxValueSelector, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return GetMaxValue<TDocument, TValue>(filter, maxValueSelector, partitionKey);
+        }
+
+        /// <summary>
+        /// Gets the minimum value of a property in a mongodb collections that is satisfying the filter.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <typeparam name="TValue">The type of the value used to order the query.</typeparam>
+        /// <param name="filter">A LINQ expression filter.</param>
+        /// <param name="minValueSelector">A property selector to order by ascending.</param>
+        /// <param name="partitionKey">An optional partition key.</param>
+        public virtual TValue GetMinValue<TDocument, TValue>(Expression<Func<TDocument, bool>> filter, Expression<Func<TDocument, TValue>> minValueSelector, string partitionKey = null)
+            where TDocument : IDocument<TKey>
+        {
+            return GetMinValue<TDocument, TValue>(filter, minValueSelector, partitionKey);
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        /// <summary>
+        /// Gets a collections for the type TDocument with the matching partition key (if any).
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <param name="partitionKey">An optional partition key.</param>
+        /// <returns>An <see cref="IMongoCollection{TDocument}"/></returns>
+        protected virtual IMongoCollection<TDocument> GetCollection<TDocument>(string partitionKey = null) where TDocument : IDocument<TKey>
+        {
+            return MongoDbContext.GetCollection<TDocument>(partitionKey);
+        }
+
+        /// <summary>
+        /// Gets a collections for a potentially partitioned document type.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <typeparam name="TKey">The type of the primary key.</typeparam>
+        /// <param name="document">The document.</param>
+        /// <returns></returns>
+        protected virtual IMongoCollection<TDocument> HandlePartitioned<TDocument>(TDocument document)
+            where TDocument : IDocument<TKey>
+        {
+            if (document is IPartitionedDocument)
+            {
+                return GetCollection<TDocument>(((IPartitionedDocument)document).PartitionKey);
+            }
+            return GetCollection<TDocument>();
+        }
+
+        /// <summary>
+        /// Gets a collections for a potentially partitioned document type.
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <typeparam name="TKey">The type of the primary key.</typeparam>
+        /// <param name="partitionKey">The collection partition key.</param>
+        /// <returns></returns>
+        protected virtual IMongoCollection<TDocument> HandlePartitioned<TDocument>(string partitionKey)
+            where TDocument : IDocument<TKey>
+        {
+            if (!string.IsNullOrEmpty(partitionKey))
+            {
+                return GetCollection<TDocument>(partitionKey);
+            }
+            return GetCollection<TDocument>();
+        }
+
+        /// <summary>
+        /// Converts a LINQ expression of TDocument, TValue to a LINQ expression of TDocument, object
+        /// </summary>
+        /// <typeparam name="TDocument">The document type.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="expression">The expression to convert</param>
+        protected static Expression<Func<TDocument, object>> ConvertExpression<TDocument, TValue>(Expression<Func<TDocument, TValue>> expression)
+        {
+            var param = expression.Parameters[0];
+            Expression body = expression.Body;
+            var convert = Expression.Convert(body, typeof(object));
+            return Expression.Lambda<Func<TDocument, object>>(convert, param);
+        }
+
+        #endregion
+    }
+}
