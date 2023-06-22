b7b23e7b920a7f2c7461fb6d991dfdfbae38dc06
Sean Garrett <sean@blacktau.com>
2023-06-22 22:06:27 +0100

    added tests for index handler

---
+++ CoreUnitTests/DataAccessTests/MongoDbIndexHandlerTests/DropIndexAsyncTests.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using CoreUnitTests.Infrastructure.Model;
+using MongoDB.Driver;
+using MongoDbGenericRepository;
+using Moq;
+using Xunit;
+
+namespace CoreUnitTests.DataAccessTests.MongoDbIndexHandlerTests;
+
+public class DropIndexAsyncTests : BaseIndexTests
+{
+    [Fact]
+    public async Task WhenIndexName_ThenDropsIndex()
+    {
+        // Arrange
+        var expectedIndexName = Fixture.Create<string>();
+        var collection = MockOf<IMongoCollection<TestDocument>>();
+        SetupContext(collection);
+
+        var indexManger = SetupIndexManager(collection, expectedIndexName);
+
+        // Act
+        await Sut.DropIndexAsync<TestDocument, Guid>(expectedIndexName);
+
+        // Assert
+        indexManger.Verify(x => x.DropOneAsync(expectedIndexName, CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task WhenIndexNameAndPartitionKey_ThenDropsIndex()
+    {
+        // Arrange
+        var expectedIndexName = Fixture.Create<string>();
+        var collection = MockOf<IMongoCollection<TestDocument>>();
+        var partitionKey = Fixture.Create<string>();
+
+        var context = SetupContext(collection);
+
+        var indexManger = SetupIndexManager(collection, expectedIndexName);
+
+        // Act
+        await Sut.DropIndexAsync<TestDocument, Guid>(expectedIndexName, partitionKey);
+
+        // Assert
+        indexManger.Verify(x => x.DropOneAsync(expectedIndexName, CancellationToken.None), Times.Once);
+        context.Verify(x => x.GetCollection<TestDocument>(partitionKey), Times.Once);
+    }
+
+    [Fact]
+    public async Task WhenIndexNameAndCancellationToken_ThenDropsIndex()
+    {
+        // Arrange
+        var expectedIndexName = Fixture.Create<string>();
+        var collection = MockOf<IMongoCollection<TestDocument>>();
+        var token = new CancellationToken(true);
+
+        SetupContext(collection);
+
+        var indexManger = SetupIndexManager(collection, expectedIndexName);
+
+        // Act
+        await Sut.DropIndexAsync<TestDocument, Guid>(expectedIndexName, cancellationToken: token);
+
+        // Assert
+        indexManger.Verify(x => x.DropOneAsync(expectedIndexName, token), Times.Once);
+    }
+
+    [Fact]
+    public async Task WhenIndexNameAndPartitionKeyAndCancellationToken_ThenDropsIndex()
+    {
+        // Arrange
+        var expectedIndexName = Fixture.Create<string>();
+        var collection = MockOf<IMongoCollection<TestDocument>>();
+        var token = new CancellationToken(true);
+        var partitionKey = Fixture.Create<string>();
+
+        var context = SetupContext(collection);
+
+        var indexManger = SetupIndexManager(collection, expectedIndexName);
+
+        // Act
+        await Sut.DropIndexAsync<TestDocument, Guid>(expectedIndexName, partitionKey, token);
+
+        // Assert
+        indexManger.Verify(x => x.DropOneAsync(expectedIndexName, token), Times.Once);
+        context.Verify(x => x.GetCollection<TestDocument>(partitionKey), Times.Once);
+    }
+
+    private Mock<IMongoDbContext> SetupContext<TDocument>(IMock<IMongoCollection<TDocument>> collection)
+    {
+        var context = MockOf<IMongoDbContext>();
+        context
+            .Setup(x => x.GetCollection<TDocument>(It.IsAny<string>()))
+            .Returns(collection.Object);
+
+        return context;
+    }
+
+    private Mock<IMongoIndexManager<TDocument>> SetupIndexManager<TDocument>(Mock<IMongoCollection<TDocument>> collection, string indexName)
+    {
+        var indexManager = MockOf<IMongoIndexManager<TDocument>>();
+        indexManager
+            .Setup(
+                x => x.CreateOneAsync(
+                    It.IsAny<CreateIndexModel<TDocument>>(),
+                    It.IsAny<CreateOneIndexOptions>(),
+                    It.IsAny<CancellationToken>()))
+            .ReturnsAsync(indexName);
+
+        collection
+            .SetupGet(x => x.Indexes)
+            .Returns(indexManager.Object);
+
+        return indexManager;
+    }
+}
--- CoreUnitTests/Infrastructure/IndexExtensions.cs
+++ CoreUnitTests/Infrastructure/IndexExtensions.cs
@@ -1,16 +1,30 @@
-using MongoDB.Bson;
+using System;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using MongoDbGenericRepository.Models;
+using Xunit.Abstractions;
 
 namespace CoreUnitTests.Infrastructure;
 
 public static class IndexExtensions
 {
+    public static bool EqualToJson<TDocument>(this IndexKeysDefinition<TDocument> keys, string json, ITestOutputHelper output)
+    {
+        var indexModelRendered = RenderIndexModelKeys(keys);
+        var result = indexModelRendered.Equals(json, StringComparison.Ordinal);
+        if (!result && output != null)
+        {
+            output.WriteLine($"Expected: {json}");
+            output.WriteLine($"Actual: {indexModelRendered}");
+        }
+
+        return result;
+    }
+
     public static bool EqualToJson<TDocument>(this IndexKeysDefinition<TDocument> keys, string json)
     {
         var indexModelRendered = RenderIndexModelKeys(keys);
-        return indexModelRendered.Equals(json, System.StringComparison.Ordinal);
+        return indexModelRendered.Equals(json, StringComparison.Ordinal);
     }
 
     public static bool EqualTo(this IndexCreationOptions x, CreateIndexOptions y) =>
@@ -28,7 +42,7 @@ public static class IndexExtensions
         x.Background == y.Background &&
         x.Version == y.Version;
 
-    public static bool EqualTo(this CreateIndexOptions x,  IndexCreationOptions y) =>
+    public static bool EqualTo(this CreateIndexOptions x, IndexCreationOptions y) =>
         x.Unique == y.Unique &&
         x.TextIndexVersion == y.TextIndexVersion &&
         x.SphereIndexVersion == y.SphereIndexVersion &&
@@ -52,6 +66,4 @@ public static class IndexExtensions
         var result = indexModelRendered.ToString();
         return result.Replace(" ", "");
     }
-
-
 }
